6b85b2a899b6307d4b4bdec4d9baaf02fbd11bda
adaxiik <a.jantula@seznam.cz>
2024-06-25 16:58:07 +0200

    added linux/mingw settings

--- build.sh
+++ build.sh
@@ -1,21 +1,32 @@
 #!/bin/sh
 
+PLATFORM=$1
+
 CC=gcc
 
-if [ ! -z "$1" ]; then
-    CC=$1
+if [ "$PLATFORM" = "linux" ]; then
+    CC=gcc
+elif [ "$PLATFORM" = "mingw" ]; then
+    CC=x86_64-w64-mingw32-gcc-posix
+else
+    echo "Invalid target"
+    exit 1
 fi
-
 
 PROJECT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 SRC_DIR=$PROJECT_PATH/src
 
 INCLUDE_DIR=$SRC_DIR/include/lua
-CFLAGS="-Wall -Wextra -Werror -std=c99 -O3 -ftree-vectorize -DMAKE_LIB -fno-stack-protector -fno-common"
-OUTPUT_DIR=$PROJECT_PATH/linux
-if [ ! -z "$2" ]; then
-    OUTPUT_DIR=$PROJECT_PATH/$2
+
+PLATFORM_FLAGS=""
+if [ "$PLATFORM" = "linux" ]; then
+    PLATFORM_FLAGS="-DLUA_USE_POSIX"
+elif [ "$PLATFORM" = "mingw" ]; then
+    PLATFORM_FLAGS="-DLUA_USE_WINDOWS"
 fi
+
+CFLAGS="-Wall -Wextra -Werror -std=c99 -O3 -ftree-vectorize -DMAKE_LIB -fno-stack-protector -fno-common"
+OUTPUT_DIR=$PROJECT_PATH/$PLATFORM
 
 mkdir -p $OUTPUT_DIR
 
